8cffaeb11d3e68dca288b64042d3a3a403dd6dba
Jamal Arif <32341971+jamalarif@users.noreply.github.com>
2020-10-22 14:30:08 -0700

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello this is a new release"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
